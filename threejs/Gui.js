4540aead10b386ca584718e03ae9be8486608913
Andreius_14 <Andreius_14@outlook.com>
2025-05-04 02:18:50 -0500

    sin novedad

--- threejs/Gui.js
+++ threejs/Gui.js
@@ -2,8 +2,17 @@ import GUI from "lil-gui";
 
 let gui;
 let mixer;
+const api = { state: "Walking" };
+//----------------------------------------//
+//            Function
+//----------------------------------------//
 
 function createGUI(model, animations) {
+  //----------------------------------------------------------------//
+  //                      VARIABLES - GUI
+  //----------------------------------------------------------------//
+
+  // GUI - Array
   const states = [
     "Idle",
     "Walking",
@@ -13,17 +22,22 @@ function createGUI(model, animations) {
     "Sitting",
     "Standing",
   ];
+  // GUI - Array
   const emotes = ["Jump", "Yes", "No", "Wave", "Punch", "ThumbsUp"];
-
-  gui = new GUI();
-
-  mixer = new THREE.AnimationMixer(model);
-
   actions = {};
 
+  //----------------------------------------------------------------//
+  //                        MIXER
+  //----------------------------------------------------------------//
+  // Instancia
+  mixer = new THREE.AnimationMixer(model);
+
+  // Recorre Animaciones
   for (let i = 0; i < animations.length; i++) {
-    const clip = animations[i];
-    const action = mixer.clipAction(clip);
+    const clip = animations[i]; //--> Primera Animacion
+    const action = mixer.clipAction(clip); //--> Carga Animacion
+
+    // Rrellena el Objeto Actions
     actions[clip.name] = action;
 
     if (emotes.indexOf(clip.name) >= 0 || states.indexOf(clip.name) >= 4) {
@@ -31,51 +45,39 @@ function createGUI(model, animations) {
       action.loop = THREE.LoopOnce;
     }
   }
+  activeAction = actions["Walking"];
+  activeAction.play();
 
-  // states
+  //----------------------------------------------------------------//
+  //                        GUI - STATES
+  //----------------------------------------------------------------//
 
+  face = model.getObjectByName("Head_4");
+
+  // GUI - Variables
+  gui = new GUI();
   const statesFolder = gui.addFolder("States");
+  const emoteFolder = gui.addFolder("Emotes");
+  const expressionFolder = gui.addFolder("Expressions");
 
+  statesFolder.open();
+  emoteFolder.open();
+  expressionFolder.open();
+
+  // De AQui en Adelante no lo entiendo
+  // GUI - Insertar Valores
   const clipCtrl = statesFolder.add(api, "state").options(states);
 
+  // Animacion - Tiempo de Demora entre cambio de animacion
   clipCtrl.onChange(function () {
     fadeToAction(api.state, 0.5);
   });
 
-  statesFolder.open();
-
-  // emotes
-
-  const emoteFolder = gui.addFolder("Emotes");
-
-  function createEmoteCallback(name) {
-    api[name] = function () {
-      fadeToAction(name, 0.2);
-
-      mixer.addEventListener("finished", restoreState);
-    };
-
-    emoteFolder.add(api, name);
-  }
-
-  function restoreState() {
-    mixer.removeEventListener("finished", restoreState);
-
-    fadeToAction(api.state, 0.2);
-  }
-
-  for (let i = 0; i < emotes.length; i++) {
-    createEmoteCallback(emotes[i]);
-  }
-
-  emoteFolder.open();
-
-  // expressions
-
-  face = model.getObjectByName("Head_4");
+  //----------------------------------------------------------------//
+  //                        Expressions
+  //----------------------------------------------------------------//
 
   const expressions = Object.keys(face.morphTargetDictionary);
-  const expressionFolder = gui.addFolder("Expressions");
 
   for (let i = 0; i < expressions.length; i++) {
     expressionFolder
@@ -83,12 +85,30 @@ function createGUI(model, animations) {
       .name(expressions[i]);
   }
 
-  activeAction = actions["Walking"];
-  activeAction.play();
+  //----------------------------------------------------------------//
+  //                        EMOTES
+  //----------------------------------------------------------------//
 
-  expressionFolder.open();
+  for (let i = 0; i < emotes.length; i++) {
+    createEmoteCallback(emotes[i]);
+  }
+
+  function createEmoteCallback(name) {
+    api[name] = function () {
+      fadeToAction(name, 0.2);
+      mixer.addEventListener("finished", restoreState);
+    };
+    emoteFolder.add(api, name);
+  }
+
+  function restoreState() {
+    mixer.removeEventListener("finished", restoreState);
+    fadeToAction(api.state, 0.2);
+  }
 }
-
+//----------------------------------------//
+//            Function
+//----------------------------------------//
 function fadeToAction(name, duration) {
   previousAction = activeAction;
   activeAction = actions[name];
