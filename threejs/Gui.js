339406c78d39ff029413810055dae5e6decd6858
Andreius_14 <Andreius_14@outlook.com>
2025-05-05 03:02:21 -0500

    nada relevante

--- threejs/Gui.js
+++ threejs/Gui.js
@@ -3,124 +3,134 @@ import GUI from "lil-gui";
 let gui;
 let mixer;
 const api = { state: "Walking" };
-//----------------------------------------//
-//            Function
-//----------------------------------------//
 
-function createGUI(model, animations) {
-  //----------------------------------------------------------------//
-  //                      VARIABLES - GUI
-  //----------------------------------------------------------------//
+// MODELO
+const rutaModelo = "./3D-Threejs/Soldier/Soldier.glb";
+[model, animations] = await cargarModeloGlb(scene, rutaModelo);
 
-  // GUI - Array
-  const states = [
-    "Idle",
-    "Walking",
-    "Running",
-    "Dance",
-    "Death",
-    "Sitting",
-    "Standing",
-  ];
-  // GUI - Array
-  const emotes = ["Jump", "Yes", "No", "Wave", "Punch", "ThumbsUp"];
-  actions = {};
+model.traverse((object) => {
+  if (object.isMesh) object.castShadow = true;
+});
 
-  //----------------------------------------------------------------//
-  //                        MIXER
-  //----------------------------------------------------------------//
-  // Instancia
-  mixer = new THREE.AnimationMixer(model);
+// MODELO - ANIMACION
+mixer = new THREE.AnimationMixer(model);
 
-  // Recorre Animaciones
-  for (let i = 0; i < animations.length; i++) {
-    const clip = animations[i]; //--> Primera Animacion
-    const action = mixer.clipAction(clip); //--> Carga Animacion
+idleAction = mixer.clipAction(animations[0]);
+walkAction = mixer.clipAction(animations[3]);
+runAction = mixer.clipAction(animations[1]);
 
-    // Rrellena el Objeto Actions
-    actions[clip.name] = action;
+actions = [idleAction, walkAction, runAction];
 
-    if (emotes.indexOf(clip.name) >= 0 || states.indexOf(clip.name) >= 4) {
-      action.clampWhenFinished = true;
-      action.loop = THREE.LoopOnce;
-    }
-  }
-  activeAction = actions["Walking"];
-  activeAction.play();
+createPanel();
+activateAllActions();
 
-  //----------------------------------------------------------------//
-  //                        GUI - STATES
-  //----------------------------------------------------------------//
+function createPanel() {
+  const panel = new GUI({ width: 310 });
 
-  face = model.getObjectByName("Head_4");
+  const folder1 = panel.addFolder("Visibility");
+  const folder2 = panel.addFolder("Activation/Deactivation");
+  const folder3 = panel.addFolder("Pausing/Stepping");
+  const folder4 = panel.addFolder("Crossfading");
+  const folder5 = panel.addFolder("Blend Weights");
+  const folder6 = panel.addFolder("General Speed");
 
-  // GUI - Variables
-  gui = new GUI();
-  const statesFolder = gui.addFolder("States");
-  const emoteFolder = gui.addFolder("Emotes");
-  const expressionFolder = gui.addFolder("Expressions");
+  folder1.open();
+  folder2.open();
+  folder3.open();
+  folder4.open();
+  folder5.open();
+  folder6.open();
 
-  statesFolder.open();
-  emoteFolder.open();
-  expressionFolder.open();
+  settings = {
+    //Folder1
+    "show model": true,
+    "show skeleton": false,
+    //Folder2
+    "deactivate all": deactivateAllActions,
+    "activate all": activateAllActions,
+    //Folder3
+    "pause/continue": pauseContinue,
+    "make single step": toSingleStepMode,
+    "modify step size": 0.05,
+    //Cross
+    "from walk to idle": () => {
+      prepareCrossFade(walkAction, idleAction, 1.0);
+    },
+    "from idle to walk": () => {
+      prepareCrossFade(idleAction, walkAction, 0.5);
+    },
+    "from walk to run": () => {
+      prepareCrossFade(walkAction, runAction, 2.5);
+    },
+    "from run to walk": () => {
+      prepareCrossFade(runAction, walkAction, 5.0);
+    },
+    //Cross Folder4
+    "use default duration": true,
+    "set custom duration": 3.5,
+    //Folder5
+    "modify idle weight": 0.0,
+    "modify walk weight": 1.0,
+    "modify run weight": 0.0,
+    //Folder6
+    "modify time scale": 1.0,
+  };
+  //-------------------------------------------------------------
+  // ↓↓↓↓↓↓↓↓↓ De Aqui para Abajo CHECK ↓↓↓↓↓↓↓↓↓↓↓↓
+  //-------------------------------------------------------------
+  //Folder1
+  folder1.add(settings, "show model").onChange(showModel); // CHECK - Booleano
+  folder1.add(settings, "show skeleton").onChange(showSkeleton); // CHECK - Booleano
+  //-------------------------------------------------------------
+  // ↓↓↓↓↓↓↓↓↓ De Aqui para Abajo son Menu Seleccionable ↓↓↓↓↓↓↓↓↓↓↓↓
+  //-------------------------------------------------------------
+  //Folder2
+  folder2.add(settings, "deactivate all");
+  folder2.add(settings, "activate all");
+  //Folder3
+  folder3.add(settings, "pause/continue");
+  folder3.add(settings, "make single step");
+  //-------------------------------------------------------------
+  // ↓↓↓↓↓↓↓↓↓ De Aqui para Abajo son Sintonizadores ↓↓↓↓↓↓↓↓↓↓↓↓
+  //-------------------------------------------------------------
+  folder3.add(settings, "modify step size", 0.01, 0.1, 0.001); //Sintonizadores
+  //-------------------------------------------------------------
+  // ↓↓↓↓↓↓↓↓↓ De Aqui para Abajo son Menu Seleccionable ↓↓↓↓↓↓↓↓↓↓↓↓
+  //-------------------------------------------------------------
+  //cross
+  crossFadeControls.push(folder4.add(settings, "from walk to idle"));
+  crossFadeControls.push(folder4.add(settings, "from idle to walk"));
+  crossFadeControls.push(folder4.add(settings, "from walk to run"));
+  crossFadeControls.push(folder4.add(settings, "from run to walk"));
+  //Cross Folder4
+  folder4.add(settings, "use default duration"); // CHECK - Booleano
 
-  // De AQui en Adelante no lo entiendo
-  // GUI - Insertar Valores
-  const clipCtrl = statesFolder.add(api, "state").options(states);
+  //-------------------------------------------------------------
+  // ↓↓↓↓↓↓↓↓↓ De Aqui para Abajo son Sintonizadores ↓↓↓↓↓↓↓↓↓↓↓↓
+  //-------------------------------------------------------------
 
-  // Animacion - Tiempo de Demora entre cambio de animacion
-  clipCtrl.onChange(function () {
-    fadeToAction(api.state, 0.5);
-  });
-
-  //----------------------------------------------------------------//
-  //                        Expressions
-  //----------------------------------------------------------------//
-
-  const expressions = Object.keys(face.morphTargetDictionary);
-
-  for (let i = 0; i < expressions.length; i++) {
-    expressionFolder
-      .add(face.morphTargetInfluences, i, 0, 1, 0.01)
-      .name(expressions[i]);
-  }
-
-  //----------------------------------------------------------------//
-  //                        EMOTES
-  //----------------------------------------------------------------//
-
-  for (let i = 0; i < emotes.length; i++) {
-    createEmoteCallback(emotes[i]);
-  }
-
-  function createEmoteCallback(name) {
-    api[name] = function () {
-      fadeToAction(name, 0.2);
-      mixer.addEventListener("finished", restoreState);
-    };
-    emoteFolder.add(api, name);
-  }
-
-  function restoreState() {
-    mixer.removeEventListener("finished", restoreState);
-    fadeToAction(api.state, 0.2);
-  }
-}
-//----------------------------------------//
-//            Function
-//----------------------------------------//
-function fadeToAction(name, duration) {
-  previousAction = activeAction;
-  activeAction = actions[name];
-
-  if (previousAction !== activeAction) {
-    previousAction.fadeOut(duration);
-  }
-
-  activeAction
-    .reset()
-    .setEffectiveTimeScale(1)
-    .setEffectiveWeight(1)
-    .fadeIn(duration)
-    .play();
+  folder4.add(settings, "set custom duration", 0, 10, 0.01);
+  //Folder5
+  folder5
+    .add(settings, "modify idle weight", 0.0, 1.0, 0.01)
+    .listen()
+    .onChange(function (weight) {
+      setWeight(idleAction, weight);
+    });
+  folder5
+    .add(settings, "modify walk weight", 0.0, 1.0, 0.01)
+    .listen()
+    .onChange(function (weight) {
+      setWeight(walkAction, weight);
+    });
+  folder5
+    .add(settings, "modify run weight", 0.0, 1.0, 0.01)
+    .listen()
+    .onChange(function (weight) {
+      setWeight(runAction, weight);
+    });
+  //Folder6
+  folder6
+    .add(settings, "modify time scale", 0.0, 1.5, 0.01)
+    .onChange(modifyTimeScale);
 }
