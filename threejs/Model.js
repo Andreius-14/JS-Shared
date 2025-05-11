49e6ccf62c3ce77f605fe65dd8e1f54bfc7e7eba
Andreius_14 <Andreius_14@outlook.com>
2025-05-11 02:42:06 -0500

    (add) Funcion que optimiza la instancia loader

--- threejs/Model.js
+++ threejs/Model.js
@@ -1,16 +1,38 @@
 import * as THREE from "three";
+// Carga Modelo
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+// Optimizador de Carga
+import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
+
 import { enableShadows } from "./_shared.js";
 //----------------------------------------------------------------//
 //                     MODELOS GLTF
 //----------------------------------------------------------------//
+export const createLoader = ({
+  // cache = false,
+  rutaDraco = "jsm/libs/draco/gltf/",
+} = {}) => {
+  const draco = new DRACOLoader();
+  const loader = new GLTFLoader();
+
+  draco.setDecoderPath(rutaDraco);
+  loader.setDRACOLoader(draco);
+
+  // if (cache) {
+  //   import("three").then(({ Cache }) => {
+  //     Cache.enabled = true;
+  //   });
+  // }
+  return loader;
+};
+
 export const loadModelGlb = (
   addToScene,
   ruta,
-  { AllElements = false } = {},
+  { AllElements = false, objLoader = null } = {},
 ) => {
   return new Promise((resolve, reject) => {
-    const loader = new GLTFLoader();
+    const loader = objLoader || new GLTFLoader();
 
     loader.load(
       ruta,
@@ -47,9 +69,11 @@ export function createSkeletonHelper(escena, modelo3D) {
 export const Model = {
   // Directo
   loadModelGlb,
+  createLoader,
   createSkeletonHelper,
   enableShadows,
   // Minimalista
+  Loader: createLoader,
   load: loadModelGlb,
   skeletonHelper: createSkeletonHelper,
 };
