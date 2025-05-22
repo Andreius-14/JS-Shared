d3a6de059c901519990fd2d03f43a2df92a4a31d
Andreius_14 <Andreius_14@outlook.com>
2025-05-22 17:51:41 -0500

    (add) funcion toArray() toRaddians() y un verificador estring()

--- threejs/_shared.js
+++ threejs/_shared.js
@@ -16,3 +16,23 @@ export function enableShadows(model, { proyecta = true, recibe = false } = {}) {
     model.receiveShadow = recibe;
   }
 }
+
+//--------------------------------------
+// INDEPENDIENTES
+//--------------------------------------
+export function toArray(variable) {
+  return Array.isArray(variable) ? variable : variable ? [variable] : [];
+}
+
+export function esString(valor) {
+  return typeof valor === "string" || valor instanceof String;
+}
+
+export function toRadians(value) {
+  return Array.isArray(value)
+    ? value.map((v) => v * (Math.PI / 180))
+    : value * (Math.PI / 180);
+}
+//--------------------------------------
+// DEPENDIENTES
+//--------------------------------------
