04900e2f69a829f8517210f6464ff2d74294ca53
Andreius_14 <Andreius_14@outlook.com>
2026-01-14 23:06:02 -0500

    (add) objeto basic, para legibilidad Interna en funcion

--- threejs/Mesh.js
+++ threejs/Mesh.js
@@ -1,13 +1,13 @@
 /* eslint indent: "off" */
 /* eslint-disable space-before-function-paren */
-import * as THREE from "three";
+import * as THREE from 'three'
 
-import { enableShadows } from "./_shared.js";
+import { enableShadows } from './_shared.js'
 
-//----------------------------------------------------------------//
+// ----------------------------------------------------------------//
 //                     Functions
-//----------------------------------------------------------------//
-export const geometriaBasica = {
+// ----------------------------------------------------------------//
+const geometriaBasica = {
     Cubo: (size = 1) => new THREE.BoxGeometry(size, size, size),
 
     Esfera: (radio = 0.7, segmentos = 32, anillos = 16) =>
@@ -22,11 +22,11 @@ export const geometriaBasica = {
     Torus: (radio = 2, tubo = 0.5, segRadiales = 16, segTubulares = 50) =>
         new THREE.TorusGeometry(radio, tubo, segRadiales, segTubulares),
 
-    Plano: (ancho = 1, alto = 1) => new THREE.PlaneGeometry(ancho, alto),
+    Plano: (ancho = 1, alto = 1) => new THREE.PlaneGeometry(ancho, alto)
     // Agrega más geometrías personalizadas según sea necesario
-};
+}
 
-export const materialesBasicos = {
+const materialesBasicos = {
     Color: () => new THREE.MeshBasicMaterial(),
     Sombra: () => new THREE.MeshStandardMaterial(),
 
@@ -36,56 +36,59 @@ export const materialesBasicos = {
 
     // Alias
     Drogas: () => new THREE.MeshNormalMaterial(),
-    Reflectante: () => new THREE.MeshPhongMaterial(),
-};
+    Reflectante: () => new THREE.MeshPhongMaterial()
+}
+
+const basic = {
+    geo: geometriaBasica,
+    mat: materialesBasicos
+}
 
 export function createGeometriaSimple(geometria, material, color) {
-    const [g, m] = [geometria, material];
-    m.color = new THREE.Color(color);
-
-    return new THREE.Mesh(g, m);
+    const [g, m] = [geometria, material]
+    m.color = new THREE.Color(color)
+    return new THREE.Mesh(g, m)
 }
 export function createGeometria(
     escena,
     {
-        geo = geometriaBasica.Esfera(),
-        material = materialesBasicos.Drogas(),
+        geo = basic.geo.Esfera(),
+        material = basic.mat.Drogas(),
         posicion = [0, 0, 0],
         rotacion = [0, 0, 0],
         escala = [1, 1, 1],
         nombre = null,
         color = null,
-        shadow = false,
-    } = {},
+        shadow = false
+    } = {}
 ) {
     // Material
-    const materialFinal = material.clone();
-    if (color) materialFinal.color = new THREE.Color(color);
+    const _material = material.clone()
+    if (color) _material.color = new THREE.Color(color)
 
     // Objeto
-    const objeto3D = new THREE.Mesh(geo, materialFinal);
-    objeto3D.position.set(...posicion);
-    objeto3D.rotation.set(...rotacion);
-    objeto3D.scale.set(...escala);
+    const objeto = new THREE.Mesh(geo, _material)
+    objeto.position.set(...posicion)
+    objeto.rotation.set(...rotacion)
+    objeto.scale.set(...escala)
 
     // Metadata
-    if (nombre) objeto3D.name = nombre;
-    if (shadow) enableShadows(objeto3D, true, true);
-    if (escena) escena.add(objeto3D);
+    if (nombre) objeto.name = nombre
+    if (shadow) enableShadows(objeto, true, true)
+    if (escena) escena.add(objeto)
 
-    return objeto3D;
+    return objeto
 }
-//-------------------------------------//
+// -------------------------------------//
 //        OBJETO UNIFICADOR
-//-------------------------------------//
-export const geo = geometriaBasica;
-export const mat = materialesBasicos;
-export const geometria3D = createGeometria;
+// -------------------------------------//
+export const geo = geometriaBasica
+export const mat = materialesBasicos
 
 export const Mesh = {
     // Directo
     enableShadows,
     // Minimalista
     create: createGeometria,
-    simple: createGeometriaSimple,
-};
+    simple: createGeometriaSimple
+}
